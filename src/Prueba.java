9e4c03b3252fe7d464dd997191ee96745dd0d2d9
Cristofer Casas <cristofer32513@gmail.com>
2018-11-16 23:35:25 -0600

    Se agrego el codigo de la Prueba.

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,9 +1,25 @@
+import java.io.IOException;
 
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BellmanFord bellman;
+		try {
+			bellman = new BellmanFord();
+			bellman.relajoArista();
+	        if (bellman.ciclo()) {
+	            for (int i = 0; i < bellman.numeroVertices; i++) {
+	                System.out.println("Coste desde " + bellman.nodoOrigen + " a " + (i + 1) + " =>" + bellman.etiquetas[i]);
+	            }
+	            for (int i = 0; i < bellman.numeroVertices; i++) {
+	                System.out.println("El predecesor de " + (i + 1) + " es " + (bellman.predecesor[i] + 1));
+	            }
+	        } else {
+	            System.out.println("Hay un ciclo negativo");
+	        }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
